19b598b1461efba0ca4d4451fbb1e68eff0583ae
Henry Hobhouse <henryhobhouse@gmail.com>
2021-07-21 22:50:54 +0100

    feat: types to requests

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,21 +1,28 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { Post } from './types';
 
-export const createNewPost = async (newPost: Omit<Post, 'id'>) =>
-  axios.post('/api/posts', newPost);
+export const createNewPost = async (newPost: Omit<Post, 'id'>) => {
+  const { data } = await axios.post<Omit<Post, 'id'>, AxiosResponse<Post>>(
+    '/api/posts',
+    newPost
+  );
+  return data;
+};
 
 export const getPostById = async (postId: string) => {
-  const { data } = await axios.get(`/api/posts/${postId}`);
+  const { data } = await axios.get<Post>(`/api/posts/${postId}`);
   return data;
 };
 
 export const deletePostById = async (postId: string) =>
-  axios.delete(`/api/posts/${postId}`);
+  axios.delete<Post>(`/api/posts/${postId}`);
 
 export const getAllPosts = async () => {
-  const { data } = await axios.get('/api/posts');
+  const { data } = await axios.get<Post[]>('/api/posts');
   return data;
 };
 
-export const updatePostById = async (post: Post) =>
-  axios.patch(`/api/posts/${post.id}`, post);
+export const updatePostById = async (post: Post) => {
+  const { data } = await axios.patch<Post>(`/api/posts/${post.id}`, post);
+  return data;
+};
