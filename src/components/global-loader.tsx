033d942cfd49db841e2ee60f2f0e911d652ad0c9
Henry Hobhouse <henryhobhouse@gmail.com>
2021-07-21 22:03:01 +0100

    feat: refetch loader

--- src/components/global-loader.tsx
+++ src/components/global-loader.tsx
@@ -5,10 +5,10 @@ export default function GlobalLoader() {
   return (
     <Loader
       style={{
-        opacity: 1,
-        position: 'absolute',
-        top: '0.5rem',
-        right: '0.5rem',
+        opacity: '1',
+        position: 'fixed',
+        bottom: '1rem',
+        right: '1rem',
         fontSize: '1.5rem',
         transition: '0.3s ease',
       }}
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,16 @@
 import React, { FC } from 'react';
 import Sidebar from './sidebar';
 import { Wrapper, Content } from './styled';
+import { useIsFetching } from 'react-query';
+import GlobalLoader from './global-loader';
 
-export const Layout: FC = ({ children }) => (
-  <Wrapper>
-    <Sidebar />
-    <Content>{children}</Content>
-  </Wrapper>
-);
+export const Layout: FC = ({ children }) => {
+  const isFetching = useIsFetching();
+  return (
+    <Wrapper>
+      <Sidebar />
+      <Content>{children}</Content>
+      {!!isFetching && <GlobalLoader />}
+    </Wrapper>
+  );
+};
