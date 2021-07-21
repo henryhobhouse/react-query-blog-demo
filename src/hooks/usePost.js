96535c34f057f0c5a4bd663a078d6d29697a1e05
Henry Hobhouse <henryhobhouse@gmail.com>
2021-07-21 19:43:33 +0100

    refactor: post query name

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -3,5 +3,5 @@ import { useQuery } from 'react-query';
 import { getPostById } from '../api/posts';
 
 export default function usePost(postId) {
-  return useQuery(['post', postId], () => getPostById(postId));
+  return useQuery(['posts', postId], () => getPostById(postId));
 }
